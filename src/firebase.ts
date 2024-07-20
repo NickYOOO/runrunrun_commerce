7f3d8dc9aabf8ec6913b5dfc7cd4ec8369f1c2d6
Nick YOOO <outgrowthr@gmail.com>
2024-07-20 15:31:49 +0900

    Router-Dom

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyD4FfTja_17odyalISsQZbvCuSb0MowTdo',
+  authDomain: 'runrunrun-af0f5.firebaseapp.com',
+  projectId: 'runrunrun-af0f5',
+  storageBucket: 'runrunrun-af0f5.appspot.com',
+  messagingSenderId: '511000634983',
+  appId: '1:511000634983:web:c7fd72a60050092642d6c2',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export { app };
