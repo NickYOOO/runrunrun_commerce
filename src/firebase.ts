5c8d4c780d2940560859ee49504533472c50caa5
Nick YOOO <outgrowthr@gmail.com>
2024-08-05 11:25:21 +0900

    Chore: fix Firebase AIP key 이슈, 환경설정 -VITE_FIREBASE_API_KEY 를 사용하여 .env에 저장 -firebase.ts 를 이용하여 타입 설정

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,14 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD4FfTja_17odyalISsQZbvCuSb0MowTdo',
-  authDomain: 'runrunrun-af0f5.firebaseapp.com',
-  projectId: 'runrunrun-af0f5',
-  storageBucket: 'runrunrun-af0f5.appspot.com',
-  messagingSenderId: '511000634983',
-  appId: '1:511000634983:web:c7fd72a60050092642d6c2',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
-
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
